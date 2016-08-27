21c5ecac6f427aa629f525488fa014ab1555d678
John Overton <John Overton>
2016-08-27 11:22:04 -0500

    neww

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+var rappers = [{id:'icecube', img:'img/icecube.jpg'}, 
+{id:'img/jayz.jpg'}, 
+{id:'img/kanyewest.jpg'},
+ {id:'img/pimpc.jpg'} ];
+
+ console.log(rappers);
+
+// function buildRappers(){
+// 	for (var i = 0; i < rappers.length; i++) {
+// 		var currentRappers = rappers[i];
+// 		&('#rappers').append();
+// 	}
+
+//}
+
+//buildRappers();
+
+
+
+//function buildCharacters(){
+//		for (var i = 0; i < rappers.length; i++1) {
+//			var currentRappers = rappers[i];
+//		$('#rappers').append('<div id=' + currentRappers.id + '></div>')
+//	}
+//}
+	
+
+
+//buildCharacters();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
